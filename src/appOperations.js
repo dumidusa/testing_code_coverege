77f9ba0a4c34372658b844fc6168b0bcd76f7e15
dumidusa <fortermux298@gmail.com>
2026-06-29 22:53:34 +0530

    add a new func

--- src/appOperations.js
+++ src/appOperations.js
@@ -1,4 +1,3 @@
-simpleToken= "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"
 function multiply(num1, num2) {
     return num1*num2;
 }
@@ -7,7 +6,11 @@ function add(num1,num2) {
     return num1+num2;
 }
 
+function substract(num1,num2){
+    return num1-num2;
+}
 module.exports = {
     multiply,
-    add
+    add,
+    substract
 };
