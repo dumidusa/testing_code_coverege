044fef3769c328f4569d8b1dc588d8577b7b6634
dumidusa <fortermux298@gmail.com>
2026-07-06 10:06:16 +0530

    fixed some issues

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,23 +1,21 @@
-const appOperations = require('../src/appOperations');
+const appOperations = require("../src/appOperations");
 
-test("multiplication property of zero", ()=>{
-    expect(appOperations.multiply(5, 0)).toBe(0);
+test("multiplication property of zero", () => {
+  expect(appOperations.multiply(5, 0)).toBe(0);
 });
 
-test("adding 2 values ", ()=>{
-    expect(appOperations.add(5, 5)).toBe(10);
+test("adding 2 values", () => {
+  expect(appOperations.add(5, 5)).toBe(10);
 });
 
-
-test("sub 2 values ", ()=>{
-    expect(appOperations.substracttracting(15, 5)).toBe(10);
+test("sub 2 values", () => {
+  expect(appOperations.substract(15, 5)).toBe(10);
 });
 
-test("devide 2 values ",()=>{
-    expect(appOperations.devide(15,5)).tobe(3);
+test("devide 2 values", () => {
+  expect(appOperations.devide(15, 5)).toBe(3);
 });
-test("mul 2 values",()=>{
-    expect(appOperations.mul(2,3)).tobe(8);
-})
 
-//change added func for handle input values
+test("mul 2 values", () => {
+  expect(appOperations.multiply(2, 3)).toBe(6);
+});
